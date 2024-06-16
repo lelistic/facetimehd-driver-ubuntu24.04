761c5a9c0dcca8f1fc794aaffc897ba7d74cb616
Claudio Lelis <68086826+lelistic@users.noreply.github.com>
2024-06-16 14:10:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ cd bcwc_pcie && sudo dkms add . ;
 
 dkms status;
 
-sudo dkms build facetimehd/0.6.8;
+sudo dkms build --force facetimehd/0.6.8;
 
-sudo dkms mktarball -m facetimehd -v 0.6.8;
-
-sudo dkms install -m facetimehd -v 0.6.8;
+sudo dkms install --force -m facetimehd -v 0.6.8;
 
 v4l2-ctl --list-devices;
 
